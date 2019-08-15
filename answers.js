a5885750ba87cb563f7eff47425553b027285f11
jskol22 <54127518+jskol22@users.noreply.github.com>
2019-08-15 11:46:00 -0400

    answers.js
    This file contains the code I felt I was confidently able to complete in the Algorithm Book

---
+++ answers.js
@@ -0,0 +1,184 @@
+PAGE 16:
+
+SETTING AND SWAPPING
+var myNumber = 42;
+var myName = Jake;
+var temp = myNumber
+myNumber = myName
+myName = temp
+
+PRINT -52 to 1066
+for(var i = -52; i < 1077; i++){
+console.log(i)
+}
+
+Don't Worry, Be Happy
+function beCheerful(){
+for(var i = 0; i < 99; i++){
+console.log('good morning!');
+}
+}
+
+Multiples of 3, But Not All
+for(var i = -300; i < 1; i++){
+if(i % 3 === 1);
+sum += i;{
+if(i == -6);
+continue;{
+if(i == -3);
+continue;
+}
+console.log(i)
+
+Printing Integers with While
+var i = 2000
+while(i < 5280){
+console.log(i)
+}
+
+You Say It's Your Birthday 
+var Day = 19
+var Month = 9
+if(Day = Month){
+console.log('How did you know?');
+}
+else
+{ console.log('Just another day....')
+}
+
+Leap Year
+for(var i = 0; i < 10000; i++){
+if(i % 4 === 0);{
+console.log(i)
+}
+}
+
+Print and Count
+var count = 0; {
+for(var i = 512; i < 4096; i++){
+if( i % 5 === 0)
+count += i;
+}
+console.log(count)
+}
+
+Multiples of Six
+for(var i = 0; i < 6001 i++){
+if(i % 6 === 0);
+console.log(i)
+}
+
+Counting the Dojo Way
+for(var i = 1; i < 101; i++){
+if( i % 5 === 0);
+console.log('Coding')}
+{
+if( i % 10 === 0);
+console.log('Dojo')}
+console.log(i)
+}
+
+What Do You Know?
+function sayHello(incoming){
+if(incoming = 'Message');
+console.log('Hello')
+}
+
+Whoa, That Suckers Huge...
+var sum = 0 {
+for( var i = -300000; i < 300000; i++){
+if(i % 2 === 1);
+sum += i
+}
+console.log(sum)
+}
+
+Countdown By Fours
+{
+var i = 2016
+while(i--){
+if(i % 4 === 0);
+console.log(i)
+}
+if(i == 0);
+break;
+}
+
+
+PAGE 20
+
+Countdown
+{
+var arr = []
+for(var i = 3; i > -1; i--){
+arr.push(i);
+}
+console.log(arr)
+}
+arr.length = 4
+
+Print and Return
+{
+var arr = [2,4];
+console.log(arr[0])
+return arr[1]
+}
+
+First Plus Length
+{
+var arr = ['what', 'now?'];
+return (arr[0] + arr.length) 
+}
+
+Values Greater than Second
+{
+var arr = [1,3,5,7,9,13];
+var i = 1;
+while(i++){
+if (arr[i] > arr[1]);
+console.log(arr[i])
+}
+if (arr[i] > arr[5]);
+break;
+}
+
+This Length, That Value
+{
+var num1 = 3;
+var num2 = 3;
+var arr = [];
+arr.length = num1;
+arr[0] = num2;
+arr[1] = num2;
+arr[2] = num2;
+if(arr.length === num2){
+console.log('Jinx!')
+}
+}
+
+Fit the First Value
+{
+var arr = [];
+if(arr[0] > arr.length){
+console.log('Too big!');
+if(arr[0] < arr.length){
+console.log('Too small!');
+if(arr[0] = arr.length){
+console.log('Just Right!')
+}
+}
+
+Biggie Size
+function makeitBig()
+{
+var arr = [-1,3,5,-10]
+for(var i = 0; i < 5; i++){
+if(arr[i] > 0);
+arr[i] = 'big'
+}
+console.log(arr)
+}
+
+
+
+
